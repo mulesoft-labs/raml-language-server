7aa995de6b7f69c28f2f12124b19e12be063211d
Denis Denisenko <denis@onpositive.com>
2017-04-19 04:01:48 +0700

    https://github.com/mulesoft/api-workbench/issues/469 fixed.

--- src/entryPoints/vscode/server.ts
+++ src/entryPoints/vscode/server.ts
@@ -42,7 +42,8 @@ connection.onInitialize((params): InitializeResult => {
                     'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
                     'A', 'B', 'C', 'D', 'E', 'F', 'G',
                     'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-                    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+                    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                    '0', '1','2','3','4','5','6','7','8','9'
                 ]
             },
             definitionProvider : true,
