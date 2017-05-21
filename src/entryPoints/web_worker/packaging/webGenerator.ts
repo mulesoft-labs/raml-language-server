3ee7e7b32d49f4a9b26d8df698f1bee490d07a10
Denis Denisenko <denis@onpositive.com>
2017-05-22 03:55:49 +0700

    Publishing browser version as npm

--- src/entryPoints/web_worker/packaging/webGenerator.ts
+++ src/entryPoints/web_worker/packaging/webGenerator.ts
@@ -1,11 +1,14 @@
 import fs = require("fs");
 import path = require("path");
 import mkdirp = require("mkdirp");
+import rimraf = require("rimraf");
+var childProcess = require('child_process');
 var webpack = require("webpack");
+
+var rootPath = path.join(__dirname, "../../../../");
 
 function createBrowserPackage(minify = false) {
     console.log("Minify: " + minify);
-    var rootPath = path.join(__dirname, "../../../../");
 
     var sourceClientFile = path.join(rootPath, "/dist/entryPoints/web_worker/client/launch.js");
     var sourceWorkerFile = path.join(rootPath, "/dist/entryPoints/web_worker/server/ramlServerWorker.js");
@@ -29,7 +32,7 @@ function createBrowserPackage(minify = false) {
  */
 function webPackForBrowser(parserRootFolder: string, sourceClientFile : string, sourceWorkerFile: string,
     targetClientFile : string, targetWorkerFile: string, minify: boolean) {
-    console.log("Preparing to Webpack browser bundle: raml-1-parser.js");
+    console.log("Preparing to Webpack browser bundle: client.js");
 
     var plugins = [];
     if (minify) {
@@ -53,7 +56,7 @@ function webPackForBrowser(parserRootFolder: string, sourceClientFile : string, 
         context: parserRootFolder,
 
         entry: {
-            bundle: relativeSourceClientFile,
+            client: relativeSourceClientFile,
             worker: relativeSourceWorkerFile
         },
 
@@ -112,4 +115,79 @@ function webPackForBrowser(parserRootFolder: string, sourceClientFile : string, 
     });
 }
 
+export function copyDirSyncRecursive(
+    to:string,
+    from:string):void{
+
+    if(path.resolve(to)==path.resolve(from)){
+        return;
+    }
+
+    if(!fs.lstatSync(from).isDirectory()){
+
+        mkdirp.sync(path.dirname(to));
+
+        var buffer = fs.readFileSync(from);
+
+        fs.writeFileSync(to, buffer);
+
+        return;
+    }
+
+    fs.readdirSync(from).forEach(x=>{
+
+        var fromChild = path.resolve(from,x);
+
+        var toChild = path.resolve(to,x);
+        copyDirSyncRecursive(toChild,fromChild);
+    });
+}
+
+function generateBrowserNpmJSON() {
+    let targetNPMPath = path.join(rootPath, "./browser_version_npm");
+
+    let targetPackageJSONPath = path.resolve(targetNPMPath, "./package.json");
+    var sourcePackageJsonPath = path.resolve(rootPath, "./package.json");
+
+    let packageJson = JSON.parse(fs.readFileSync(sourcePackageJsonPath).toString());
+
+    let targetJson : any = {};
+    targetJson.version = packageJson.version;
+    targetJson.name = packageJson.name + '-browser';
+    targetJson.main = "client.bundle.js";
+
+    fs.writeFileSync(targetPackageJSONPath, JSON.stringify(targetJson, null, '\t'));
+}
+
+function createBrowserNPM() {
+    let browserVersionPath = path.join(rootPath, "./browserVersion")
+    let browserNpmPath = path.join(rootPath, "./browser_version_npm")
+
+    rimraf.sync(browserNpmPath);
+
+    mkdirp.sync(browserNpmPath);
+
+    generateBrowserNpmJSON();
+
+    copyDirSyncRecursive(browserNpmPath, browserVersionPath);
+}
+
+function publishBrowserNPM() {
+    let browserNpmPath = path.join(rootPath, "./browser_version_npm")
+    childProcess.execSync('cd ' + browserNpmPath + ' && npm publish');
+}
+
+function createAndPublishBrowserNPM() {
+
+    createBrowserNPM();
+    publishBrowserNPM();
+}
+
 createBrowserPackage();
+
+declare var process: any;
+var isNpm = process.argv[process.argv.indexOf("--type") + 1] === 'npm';
+
+if (isNpm) {
+    createAndPublishBrowserNPM();
+}
